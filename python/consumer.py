800e7bc8231b2e4879d209a0f0f3d48256899273
johnny <mmmaaaxxx77@gmail.com>
2019-09-12 01:44:40 +0800

    modified:   docker-compose.yml

--- python/consumer.py
+++ python/consumer.py
@@ -2,9 +2,9 @@ from kafka import KafkaConsumer
 from json import loads
 
 consumer = KafkaConsumer(
-    'topic4',
+    'topic6',
     bootstrap_servers=['35.201.148.193:9094'],
-    group_id='topic4')
+    group_id='topic6')
 
 for message in consumer:
     message = message.value
